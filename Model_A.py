be1449f2838acfdb698b09156d6c8f81c37767a8
tanvirtin <tanvir.tinz@gmail.com>
2017-12-08 20:17:32 -0500

    project completed

--- Model_A.py
+++ Model_A.py
@@ -23,7 +23,9 @@ class Model_A(object):
 
         self.batch_size = 256
 
-        self.keep_probability = 0.5
+        self.keep_probability = 0.4
+
+        self.name = "Model-A"
 
     def create_model(self):
 
@@ -34,17 +36,37 @@ class Model_A(object):
         keep_prob = tf.placeholder(tf.float32, name = "keep_prob")
 
         #############################
+        #    Convolutional layer 0  #
+        #############################
+        num_filters_l0 = 12
+        # tf variable for weights in the convolutional layer
+        #                                                               3 since our image has 3 channels
+        conv_layer_0_weights_tf = tf.Variable(tf.truncated_normal([3, 3, self.channel, num_filters_l0], stddev=0.05, mean=0.0)) # 24 is the number of filteres being applied
+        # tf variable for the bias weights
+        conv_layer_0_bias_tf = tf.Variable(tf.zeros(num_filters_l0)) # 24 is the number of outputs produced by the convolutional layer
+
+        # x is the input layer
+        conv_layer_0_unbiased = tf.nn.conv2d(x, conv_layer_0_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
+        conv_layer_0_biased = tf.nn.bias_add(conv_layer_0_unbiased, conv_layer_0_bias_tf)
+        conv_layer_0_activated = tf.nn.relu(conv_layer_0_biased)
+
+        # now I pool the convolutional layer by taking out the max value from specific windows and the pool size is 2 by 2
+        conv_layer_0_pooled = tf.nn.max_pool(conv_layer_0_activated, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
+
+
+        #############################
         #    Convolutional layer 1  #
         #############################
 
+        num_filters_l1 = 24
         # tf variable for weights in the convolutional layer
-        conv_layer_1_weights_tf = tf.Variable(tf.truncated_normal([3, 3, x.get_shape().as_list()[3], 24], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
+        #                                                               3 since our image has 3 channels
+        conv_layer_1_weights_tf = tf.Variable(tf.truncated_normal([3, 3, num_filters_l0, num_filters_l1], stddev=0.05, mean=0.0)) # 24 is the number of filteres being applied
         # tf variable for the bias weights
-        conv_layer_1_bias_tf = tf.Variable(tf.zeros(24)) # 24 is the number of outputs produced by the convolutional layer
+        conv_layer_1_bias_tf = tf.Variable(tf.zeros(num_filters_l1)) # 24 is the number of outputs produced by the convolutional layer
 
         # x is the input layer
-        conv_layer_1_unbiased = tf.nn.conv2d(x, conv_layer_1_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
+        conv_layer_1_unbiased = tf.nn.conv2d(conv_layer_0_pooled, conv_layer_1_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
         conv_layer_1_biased = tf.nn.bias_add(conv_layer_1_unbiased, conv_layer_1_bias_tf)
         conv_layer_1_activated = tf.nn.relu(conv_layer_1_biased)
 
@@ -56,11 +78,12 @@ class Model_A(object):
         #    Convolutional layer 2  #
         #############################
 
+        num_filters_l2 = 48
         # tf variable for weights in the convolutional layer
-        conv_layer_2_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_1_pooled.get_shape().as_list()[3], 48], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
+        #                                                               x.get_shape().as_list()[3] should be 24 since the filtered image spitted out by the previous convolutional layer should have 24 images all with 1 channel or 1 image with 24 channel
+        conv_layer_2_weights_tf = tf.Variable(tf.truncated_normal([3, 3, num_filters_l1, num_filters_l2], stddev=0.05, mean=0.0)) # 48 is the number of filters being applied
         # tf variable for the bias weights
-        conv_layer_2_bias_tf = tf.Variable(tf.zeros(48)) # 48 is the number of outputs produced by the convolutional layer
+        conv_layer_2_bias_tf = tf.Variable(tf.zeros(num_filters_l2)) # 48 is the number of outputs produced by the convolutional layer
 
         # x is the input layer
         conv_layer_2_unbiased = tf.nn.conv2d(conv_layer_1_pooled, conv_layer_2_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
@@ -74,11 +97,11 @@ class Model_A(object):
         #    Convolutional layer 3  #
         #############################
 
+        num_filters_l3 = 96
         # tf variable for weights in the convolutional layer
-        conv_layer_3_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_2_pooled.get_shape().as_list()[3], 96], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
+        conv_layer_3_weights_tf = tf.Variable(tf.truncated_normal([3, 3, num_filters_l2, num_filters_l3], stddev=0.05, mean=0.0))
         # tf variable for the bias weights
-        conv_layer_3_bias_tf = tf.Variable(tf.zeros(96)) # 48 is the number of outputs produced by the convolutional layer
+        conv_layer_3_bias_tf = tf.Variable(tf.zeros(num_filters_l3)) # 48 is the number of outputs produced by the convolutional layer
 
         # x is the input layer
         conv_layer_3_unbiased = tf.nn.conv2d(conv_layer_2_pooled, conv_layer_3_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
@@ -95,11 +118,12 @@ class Model_A(object):
 
         flatten_layer_1 = tf.contrib.layers.flatten(conv_layer_3_pooled)
 
+        fc_neurons = 512
         ###############################
         #    fully connected layer 1  #
         ###############################
 
-        fully_connected_layer_1_weights_tf = tf.Variable(tf.truncated_normal([flatten_layer_1.get_shape().as_list()[1], 512], stddev = 0.05, mean = 0.0))
+        fully_connected_layer_1_weights_tf = tf.Variable(tf.truncated_normal([flatten_layer_1.get_shape().as_list()[1], fc_neurons], stddev = 0.05, mean = 0.0))
 
         # one bias for each layer therefore 512 biasses
         fully_connected_layer_1_bias_tf = tf.Variable(tf.zeros(512))
@@ -114,7 +138,7 @@ class Model_A(object):
         #    fully connected layer 2  #
         ###############################
 
-        fully_connected_layer_2_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_1_dropped.get_shape().as_list()[1], 512], stddev = 0.05, mean = 0.0))
+        fully_connected_layer_2_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_1_dropped.get_shape().as_list()[1], fc_neurons], stddev = 0.05, mean = 0.0))
 
         # one bias for each layer therefore 512 biasses
         fully_connected_layer_2_bias_tf = tf.Variable(tf.zeros(512))
@@ -129,50 +153,82 @@ class Model_A(object):
         #  logits layer  #
         ##################
 
-        output_layer_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_2_dropped.get_shape().as_list()[1], 10], stddev = 0.05, mean = 0.0))
+        output_layer_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_2_dropped.get_shape().as_list()[1], self.classes], stddev = 0.05, mean = 0.0))
 
         # one bias for each layer therefore 10 biasses
-        output_layer_bias_tf = tf.Variable(tf.zeros(10))
+        output_layer_bias_tf = tf.Variable(tf.zeros(self.classes))
 
         # logits also known as the output of the model
         logits = tf.add(tf.matmul(fully_connected_layer_2_dropped, output_layer_weights_tf), output_layer_bias_tf)
+
+        print("Model-A created...")
 
         return (x, y, keep_prob, logits)
 
 
     def train(self):
         epochs = 15
+        average_accuracy = 0
 
         # Remove previous weights, bias, inputs, etc..
         tf.reset_default_graph()
 
         x, y, keep_prob, logits = self.create_model()
 
-        # loss of the neural network
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+        with tf.name_scope("cost"):
+            # loss of the neural network
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = y))
 
-        # optimizer will take in loss and minimize it using gradient descent
-        optimizer = tf.train.AdamOptimizer().minimize(loss)
+            # optimizer will take in loss and minimize it using gradient descent
+            optimizer = tf.train.AdamOptimizer().minimize(loss)
 
-        # Accuracy of the model
-        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+            tf.summary.scalar("cost", loss)
+
+        with tf.name_scope("accuracy"):
+            # Accuracy of the model
+            correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name = 'accuracy')
+
+            tf.summary.scalar("accuracy", accuracy)
 
         # TRAINING WILL BEGIN NOW
         print("Initiating tensorflow session...")
         with tf.Session() as sess:
+            writer = tf.summary.FileWriter("./logs/" + self.name, sess.graph)
+            merged = tf.summary.merge_all()
+
             sess.run(tf.global_variables_initializer())
 
+            begin = 0
+            finish = 256
+
             for i in range(epochs):
-                training_batch = zip(range(0, len(self.train_x), 128), range(self.batch_size, len(self.train_x)+1, 128))
+                print("On epoch no.{}".format(i + 1))
+
+                training_batch = zip(range(0, len(self.train_x), 128), range(self.batch_size, len(self.train_x) + 1, 128))
 
                 for start, end in tqdm(training_batch):
-                    sess.run(optimizer, feed_dict={x: self.train_x[start:end], y: self.train_y[start:end], keep_prob: self.keep_probability})
+                    sess.run(optimizer, feed_dict = {x: self.train_x[start:end], y: self.train_y[start:end], keep_prob: self.keep_probability})
 
 
                 # For printing purposes
-                loss = sess.run(loss, feed_dict={x: self.train_x[start:end], y: self.train_y[start:end], keep_prob: 1.})
+                l = sess.run(loss, feed_dict = {x: self.train_x[start:end], y: self.train_y[start:end], keep_prob: 1.})
 
-                valid_acc = sess.run(accuracy, feed_dict={x: self.test_x[:256], y: self.test_y[:256], keep_prob: 1.})
+                summary, valid_acc = sess.run([merged, accuracy], feed_dict = {x: self.test_x[begin:finish], y: self.test_y[begin:finish], keep_prob: 1.})
 
-                print('Loss: {:>10.4f} Accuracy: {}'.format(loss, valid_acc))
+                writer.add_summary(summary, i)
+
+                print("Loss: {:>10.4f} Accuracy: {}".format(l, valid_acc))
+
+                average_accuracy += valid_acc
+
+                begin = finish
+                finish += 256
+
+                # we break the loop if we run out of testing data
+                if (begin > len(self.test_x)):
+                    break
+
+            average_accuracy /= epochs
+
+            print("Average accuracy of the network: {}".format(average_accuracy))
--- Model_B.py
+++ Model_B.py
@@ -10,6 +10,7 @@ class Model_B(Model_A):
         # base class initializer syntax
         super().__init__()
         self.keep_probability = 0.25
+        self.name = "Model-B"
 
     def create_model(self):
 
@@ -23,159 +24,90 @@ class Model_B(Model_A):
         #    Convolutional layer 1  #
         #############################
 
+        num_filters_l1 = 24
         # tf variable for weights in the convolutional layer
-        conv_layer_1_weights_tf = tf.Variable(tf.truncated_normal([3, 3, x.get_shape().as_list()[3], 48], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
+        conv_layer_1_weights_tf = tf.Variable(tf.truncated_normal([2, 2, self.channel, num_filters_l1], stddev=0.01, mean=0.0)) # 24 is the number of filters being applied
         # tf variable for the bias weights
-        conv_layer_1_bias_tf = tf.Variable(tf.zeros(48)) # 48 is the number of outputs produced by the convolutional layer
+        conv_layer_1_bias_tf = tf.Variable(tf.zeros(num_filters_l1))
 
         # x is the input layer
         conv_layer_1_unbiased = tf.nn.conv2d(x, conv_layer_1_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
         conv_layer_1_biased = tf.nn.bias_add(conv_layer_1_unbiased, conv_layer_1_bias_tf)
         conv_layer_1_activated = tf.nn.relu(conv_layer_1_biased)
-
+        conv_layer_1_pooled = tf.nn.max_pool(conv_layer_1_activated, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
 
         #############################
         #    Convolutional layer 2  #
         #############################
 
-        # tf variable for weights in the convolutional layer
-        conv_layer_2_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_1_activated.get_shape().as_list()[3], 48], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
+        num_filters_l2 = 24
 
+        # tf variable for weights in the convolutional layer
+        conv_layer_2_weights_tf = tf.Variable(tf.truncated_normal([3, 3, num_filters_l1, num_filters_l2], stddev=0.01, mean=0.0)) # 24 is the number of filters being applied
         # tf variable for the bias weights
-        conv_layer_2_bias_tf = tf.Variable(tf.zeros(48)) # 48 is the number of outputs produced by the convolutional layer
+        conv_layer_2_bias_tf = tf.Variable(tf.zeros(num_filters_l2))
 
         # x is the input layer
-        conv_layer_2_unbiased = tf.nn.conv2d(conv_layer_1_activated, conv_layer_2_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
+        conv_layer_2_unbiased = tf.nn.conv2d(conv_layer_1_pooled, conv_layer_2_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
         conv_layer_2_biased = tf.nn.bias_add(conv_layer_2_unbiased, conv_layer_2_bias_tf)
         conv_layer_2_activated = tf.nn.relu(conv_layer_2_biased)
 
         # now I pool the convolutional layer by taking out the max value from specific windows size is 2 by 2
         conv_layer_2_pooled = tf.nn.max_pool(conv_layer_2_activated, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
 
-        # I dropout 0.25 of the conv layer
-        conv_layer_2_dropped = tf.nn.dropout(conv_layer_2_pooled, keep_prob)
-
-        #############################
-        #    Convolutional layer 3  #
-        #############################
-
-        # tf variable for weights in the convolutional layer
-        conv_layer_3_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_2_dropped.get_shape().as_list()[3], 96], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
-        # tf variable for the bias weights
-        conv_layer_3_bias_tf = tf.Variable(tf.zeros(96)) # 48 is the number of outputs produced by the convolutional layer
-
-        # x is the input layer
-        conv_layer_3_unbiased = tf.nn.conv2d(conv_layer_2_dropped, conv_layer_3_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
-        conv_layer_3_biased = tf.nn.bias_add(conv_layer_3_unbiased, conv_layer_3_bias_tf)
-        conv_layer_3_activated = tf.nn.relu(conv_layer_3_biased)
-
-
-        #############################
-        #    Convolutional layer 4  #
-        #############################
-
-        # tf variable for weights in the convolutional layer
-        conv_layer_4_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_3_activated.get_shape().as_list()[3], 96], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
-        # tf variable for the bias weights
-        conv_layer_4_bias_tf = tf.Variable(tf.zeros(96)) # 48 is the number of outputs produced by the convolutional layer
-
-        conv_layer_4_unbiased = tf.nn.conv2d(conv_layer_3_activated, conv_layer_4_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
-        conv_layer_4_biased = tf.nn.bias_add(conv_layer_4_unbiased, conv_layer_4_bias_tf)
-        conv_layer_4_activated = tf.nn.relu(conv_layer_4_biased)
-
-        # now I pool the convolutional layer by taking out the max value from specific windows size is 2 by 2
-        conv_layer_4_pooled = tf.nn.max_pool(conv_layer_4_activated, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
-
-        # I dropout 0.25 of the conv layer
-        conv_layer_4_dropped = tf.nn.dropout(conv_layer_4_pooled, keep_prob)
-
-
-        #############################
-        #    Convolutional layer 5  #
-        #############################
-
-        # tf variable for weights in the convolutional layer
-        conv_layer_5_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_4_dropped.get_shape().as_list()[3], 192], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
-        # tf variable for the bias weights
-        conv_layer_5_bias_tf = tf.Variable(tf.zeros(192)) # 48 is the number of outputs produced by the convolutional layer
-
-        # x is the input layer
-        conv_layer_5_unbiased = tf.nn.conv2d(conv_layer_4_dropped, conv_layer_5_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
-        conv_layer_5_biased = tf.nn.bias_add(conv_layer_5_unbiased, conv_layer_5_bias_tf)
-        conv_layer_5_activated = tf.nn.relu(conv_layer_5_biased)
-
-
-        #############################
-        #    Convolutional layer 6  #
-        #############################
-
-        # tf variable for weights in the convolutional layer
-        conv_layer_6_weights_tf = tf.Variable(tf.truncated_normal([3, 3, conv_layer_5_activated.get_shape().as_list()[3], 192], stddev=0.05, mean=0.0))  # 24 is the number of outputs produced by the convolutional layer
-
-        # tf variable for the bias weights
-        conv_layer_6_bias_tf = tf.Variable(tf.zeros(192)) # 48 is the number of outputs produced by the convolutional layer
-
-        conv_layer_6_unbiased = tf.nn.conv2d(conv_layer_5_activated, conv_layer_6_weights_tf, strides=[1, 1, 1, 1], padding = 'SAME')
-        conv_layer_6_biased = tf.nn.bias_add(conv_layer_6_unbiased, conv_layer_6_bias_tf)
-        conv_layer_6_activated = tf.nn.relu(conv_layer_6_biased)
-
-        # now I pool the convolutional layer by taking out the max value from specific windows size is 2 by 2
-        conv_layer_6_pooled = tf.nn.max_pool(conv_layer_6_activated, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding = 'SAME')
-
-        # I dropout 0.25 of the conv layer
-        conv_layer_6_dropped = tf.nn.dropout(conv_layer_6_pooled, keep_prob)
-
 
         #######################
         #    flatten layer 1  #
         #######################
 
-        flatten_layer_1 = tf.contrib.layers.flatten(conv_layer_6_dropped)
+        flatten_layer_1 = tf.contrib.layers.flatten(conv_layer_2_pooled)
 
         ###############################
         #    fully connected layer 1  #
         ###############################
 
-        fully_connected_layer_1_weights_tf = tf.Variable(tf.truncated_normal([flatten_layer_1.get_shape().as_list()[1], 512], stddev = 0.05, mean = 0.0))
+        fc_neurons = 512
+
+        fully_connected_layer_1_weights_tf = tf.Variable(tf.truncated_normal([num_filters_l2, fc_neurons], stddev = 0.01, mean = 0.0))
 
         # one bias for each layer therefore 512 biasses
-        fully_connected_layer_1_bias_tf = tf.Variable(tf.zeros(512))
+        fully_connected_layer_1_bias_tf = tf.Variable(tf.zeros(fc_neurons))
 
         fully_connected_layer_1_output = tf.add(tf.matmul(flatten_layer_1, fully_connected_layer_1_weights_tf), fully_connected_layer_1_bias_tf)
 
         fully_connected_layer_1_activated = tf.nn.relu(fully_connected_layer_1_output)
 
-        fully_connected_layer_1_dropped = tf.nn.dropout(fully_connected_layer_1_activated, 0.5)
+        fully_connected_layer_1_dropped = tf.nn.dropout(fully_connected_layer_1_activated, keep_prob)
 
         ###############################
         #    fully connected layer 2  #
         ###############################
 
-        fully_connected_layer_2_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_1_dropped.get_shape().as_list()[1], 256], stddev = 0.05, mean = 0.0))
+        fc_neurons = 256
+
+        fully_connected_layer_2_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_1_dropped.get_shape().as_list()[1], fc_neurons], stddev = 0.01, mean = 0.0))
 
         # one bias for each layer therefore 512 biasses
-        fully_connected_layer_2_bias_tf = tf.Variable(tf.zeros(256))
+        fully_connected_layer_2_bias_tf = tf.Variable(tf.zeros(fc_neurons))
 
         fully_connected_layer_2_output = tf.add(tf.matmul(fully_connected_layer_1_dropped, fully_connected_layer_2_weights_tf), fully_connected_layer_2_bias_tf)
 
         fully_connected_layer_2_activated = tf.nn.relu(fully_connected_layer_2_output)
 
-        fully_connected_layer_2_dropped = tf.nn.dropout(fully_connected_layer_2_activated, 0.5)
+        fully_connected_layer_2_dropped = tf.nn.dropout(fully_connected_layer_2_activated, keep_prob)
 
         ##################
         #  logits layer  #
         ##################
 
-        output_layer_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_2_dropped.get_shape().as_list()[1], 10], stddev = 0.05, mean = 0.0))
+        output_layer_weights_tf = tf.Variable(tf.truncated_normal([fully_connected_layer_2_dropped.get_shape().as_list()[1], self.classes], stddev = 0.01, mean = 0.0))
 
         # one bias for each layer therefore 10 biasses
-        output_layer_bias_tf = tf.Variable(tf.zeros(10))
+        output_layer_bias_tf = tf.Variable(tf.zeros(self.classes))
 
         # logits also known as the output of the model
         logits = tf.add(tf.matmul(fully_connected_layer_2_dropped, output_layer_weights_tf), output_layer_bias_tf)
+
+        print("Model-B created...")
 
         return (x, y, keep_prob, logits)
