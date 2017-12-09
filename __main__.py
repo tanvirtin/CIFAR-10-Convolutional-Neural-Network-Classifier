3dceaf37dc22cf3f9d301d43e121abb81dec1f98
tanvirtin <tanvir.tinz@gmail.com>
2017-12-09 14:52:38 -0500

    Project completed

--- __main__.py
+++ __main__.py
@@ -1,14 +1,11 @@
-from Preprocess import Preprocess
 from Model_A import Model_A
 from Model_B import Model_B
 
 def main():
-    # model_A = Model_A()
-    #
-    # model_A.train()
+    model_A = Model_A()
+    model_A.train()
 
     model_B = Model_B()
-
     model_B.train()
 
 
